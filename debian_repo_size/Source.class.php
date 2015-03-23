aa88b187667f2ce8b49444c399c4a05209eae67c
olaulau <olaulau@gmail.com>
2015-03-23 02:50:11 +0100

    sources.list file reading

---
+++ debian_repo_size/Source.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class Source {
+	
+	private $deb;
+	private $uri;
+	private $dist;
+	private $component;
+	
+	public static function get_sources_from_line($line) {
+		$res = explode(' ', $line, 4);
+		$components = explode(' ', $res[3]);
+		$sources = array();
+		foreach ($components as $component) {
+			$src = new Source();
+			$src->deb = $res[0];
+			$src->uri = $res[1];
+			$src->dist = $res[2];
+			$src->component = $component;
+			$sources[] = $src;
+		}
+		return $sources;
+	}
+	
+// 	public 
+	
+}
+
+// $line = 'deb http://archive.ubuntu.com/ubuntu/ utopic main restricted universe multiverse';
+// $sources = Source::get_sources_from_line($line);
+// print_r($sources);
+// die;
+
+?>
---
+++ debian_repo_size/SourcesList.class.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once './Source.class.php';
+
+class SourcesList {
+	
+	private $list = array();
+	
+	public static function read_file($filename) {
+		$sl = new SourcesList();
+		$file = new SplFileObject($filename);
+		while (!$file->eof()) {
+			$line = trim($file->fgets());
+			if(strpos($line, '#') !== 0  &&  $line !== '') { // line not commented with a '#' at start
+// 				echo "processing line : $line" . PHP_EOL;
+				$sl->list = array_merge($sl->list, Source::get_sources_from_line($line));
+			}
+		}
+		return $sl;
+	}
+	
+}
+
+// $filename = 'sources.list';
+// $sl = SourcesList::read_file($filename);
+// print_r($sl);
+// die;
+
+?>
