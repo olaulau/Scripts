90ab3e89473abad11f2bc9fe1288db26965cb97d
olaulau <olaulau@gmail.com>
2015-03-24 02:36:11 +0100

    construct packages file URL, in order to automaticallly download those files

--- debian_repo_size/Source.class.php
+++ debian_repo_size/Source.class.php
@@ -22,13 +22,34 @@ class Source {
 		return $sources;
 	}
 	
-// 	public 
+	private function get_arch_string() {
+		if($this->deb === 'deb-src') {
+			return 'source';
+		}
+		else {
+			$tab = explode('-', $this->deb);
+			if(isset($tab[1]))
+				$arch = $tab[1];
+			else
+				$arch = 'i386';
+			return 'binary-' . $arch;
+		}
+	}
+	
+ 	public function get_packages_file_url() {
+ 		return $this->uri . '/dists/' . $this->dist . '/' . $this->component . '/' . $this->get_arch_string() . '/' . 'Packages.bz2'; 
+ 	}
 	
 }
 
 // $line = 'deb http://archive.ubuntu.com/ubuntu/ utopic main restricted universe multiverse';
 // $sources = Source::get_sources_from_line($line);
+
 // print_r($sources);
 // die;
+
+// foreach ($sources as $src) {
+// 	echo $src->get_packages_file_url() . PHP_EOL;
+// }
 
 ?>
