8b97a067b024ae726f21c21b58a9d823ad63db9d
olaulau <olaulau@gmail.com>
2015-03-25 01:58:38 +0100

    download and extract Packages.bz2 file for a source

--- debian_repo_size/Source.class.php
+++ debian_repo_size/Source.class.php
@@ -7,6 +7,10 @@ class Source {
 	private $dist;
 	private $component;
 	
+	public static $packages_filename = 'Packages.bz2';
+	public static $default_arch = 'i386';
+	
+	
 	public static function get_sources_from_line($line) {
 		$res = explode(' ', $line, 4);
 		$components = explode(' ', $res[3]);
@@ -22,6 +26,7 @@ class Source {
 		return $sources;
 	}
 	
+	
 	private function get_arch_string() {
 		if($this->deb === 'deb-src') {
 			return 'source';
@@ -31,19 +36,46 @@ class Source {
 			if(isset($tab[1]))
 				$arch = $tab[1];
 			else
-				$arch = 'i386';
+				$arch = Source::$default_arch;
 			return 'binary-' . $arch;
 		}
 	}
 	
+	
  	public function get_packages_file_url() {
- 		return $this->uri . '/dists/' . $this->dist . '/' . $this->component . '/' . $this->get_arch_string() . '/' . 'Packages.bz2'; 
+ 		return $this->uri . '/dists/' . $this->dist . '/' . $this->component . '/' . $this->get_arch_string() . '/' . Source::$packages_filename; 
  	}
 	
+ 	
+ 	public function download_packages_file() {
+ 		$url = $this->get_packages_file_url();
+ 		$ch = curl_init();
+ 		curl_setopt($ch, CURLOPT_URL,$url);
+ 		
+ 		$fp = fopen(Source::$packages_filename, 'w');
+ 		curl_setopt($ch, CURLOPT_FILE, $fp);
+ 		
+ 		curl_setopt($ch, CURLOPT_HEADER, 0);
+ 		curl_exec($ch);
+ 		curl_close($ch);
+ 	}
+ 	
+ 	
+ 	public function uncompress_packages_file() {
+		$sfp = bzopen(Source::$packages_filename, "r");
+ 		$fp = fopen('Packages', "w");
+ 		while (!feof($sfp)) {
+ 			$string = bzread($sfp, 4096);
+ 			fwrite($fp, $string, strlen($string));
+ 		}
+ 		bzclose($sfp);
+ 		fclose($fp);
+ 	}
 }
 
-// $line = 'deb http://archive.ubuntu.com/ubuntu/ utopic main restricted universe multiverse';
-// $sources = Source::get_sources_from_line($line);
+
+$line = 'deb http://archive.ubuntu.com/ubuntu/ utopic main restricted universe multiverse';
+$sources = Source::get_sources_from_line($line);
 
 // print_r($sources);
 // die;
@@ -51,5 +83,9 @@ class Source {
 // foreach ($sources as $src) {
 // 	echo $src->get_packages_file_url() . PHP_EOL;
 // }
+
+// $sources[0]->download_packages_file();
+// $sources[0]->uncompress_packages_file();
+
 
 ?>
