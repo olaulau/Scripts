b3090180f2f2d46ce5530358d2141f1169101fa1
laulau <olaulau@gmail.com>
2019-10-15 13:50:34 +0200

    mysql admin create script

---
+++ MYSQL/create_admin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "new mysql admin password ? "
+read -s password
+echo
+
+sql="
+DROP USER IF EXISTS admin@localhost;
+CREATE USER 'admin'@'localhost' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;"
+file=`tempfile`
+echo "$sql" > $file
+
+sudo mysql -u root < $file
+rm $file
