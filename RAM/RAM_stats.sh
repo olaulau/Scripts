778b87301ce75697b063528d5179b1bd4af2f25c
olaulau <olaulau@gmail.com>
2023-05-07 01:46:29 +0200

    RAM stats with termsql (group by powa)

--- RAM/RAM_stats.sh
+++ RAM/RAM_stats.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-COLUMNS=$(tput cols)
-ps -e -orss=,args= | sort -r -b -n | head -n10 | awk '{ printf("%d %s\n", $1/1024, $2)  }'
-# | /usr/bin/pr -TW$COLUMNS
-
-## TODO
-# ajouter PID avant la commande
-# commande : couper après premier espace, et couper avant dernier slash
-#  -> ne garder que firefox, java, csgo_linux
-
-
-## faire un autre script qui lance ca périodiquement vers une db
-## et un autre qui génère des graphs pour permettre d'analyser ce qu'il se passe.
-
+ps -e -o rss,comm | termsql --separator "\t" --columns 'rss,comm' "SELECT SUM(rss)/1024 AS ram,comm FROM tbl GROUP BY comm ORDER BY ram DESC LIMIT 10"
