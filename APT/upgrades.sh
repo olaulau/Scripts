b556f9fe407e82c25e86c9d191fa0c2343cdc39c
olaulau <olaulau@gmail.com>
2024-04-29 10:18:40 +0200

    not auto full upgrade (can really brake a system)

--- APT/upgrades.sh
+++ APT/upgrades.sh
@@ -2,9 +2,12 @@
 
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
-
 apt-fast update
-apt-fast -y full-upgrade
+apt-fast -y upgrade
+
+unset DEBIAN_FRONTEND
+unset NEEDRESTART_MODE
+apt-fast full-upgrade
+
 apt -y autoremove
 apt autoclean
-
